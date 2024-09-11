fc70b1553d8e100790bc612cf9eb2554b95e906e
gdjolt8 <triplernil4@gmail.com>
2024-09-11 18:51:41 -0400

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,57 @@
-import Image from "next/image";
+import { useState } from "react";
 
-export default function Home() {
+
+function App() {
+  let [msg, setMsg] = useState("");
+  let [name, setName] = useState("");
+  let [pass, setPass] = useState("");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+    <div className="bg-slate-200 rounded-md mt-11 w-[400px] h-[600px] shadow-md pt-5 pb-7 mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center text-blue-500 italic">
+        Bacefook
+      </h1>
+      <input
+        type="text"
+        placeholder="Enter username here"
+        className="outline-1 outline-blue-300 outline-offset-0 border-slate-200 border-2 w-[350px] outline-0 rounded-md text-base px-3.5 py-4 ml-2 mt-4"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Enter password here"
+        className="outline-1 outline-blue-300 outline-offset-0 border-slate-200 border-2 w-[350px] outline-0 rounded-md text-base px-3.5 py-4 ml-2 mt-4"
+        onChange={(e) => {
+          setPass(e.target.value);
+        }}
+      />
+      <button
+        className="outline-1 outline-blue-300 outline-offset-0 bg-blue-500 hover:bg-blue-400 border-0 rounded-md text-2xl ml-2 mt-4 py-4 w-[350px] text-white font-bold"
+        onClick={function (e) {
+          fetch("https://httpbin.org/post", {
+            method: "POST",
+            body: `username=${name}&password=${pass}`,
+          })
+            .then((res) => res.json())
+            .then((data) => setMsg(JSON.stringify(data)));
+        }}
+      >
+        Log in
+      </button>
+      <div className="mt-4 text-center">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="text-blue-600 text-lg hover:underline"
+          href="https://www.google.com"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          Forgot password?
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <p className="text-xs text-black ml-2 mt-6">
+        Bacefook will be uses for some activities later
+      </p>
     </div>
   );
 }
+
+export default App;
