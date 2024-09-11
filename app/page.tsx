32f51e2c77fa1a66b041adc56e263681a3145e50
gdjolt8 <102922030+gdjolt8@users.noreply.github.com>
2024-09-11 19:13:41 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import { useState } from "react";
 
 
 function App() {
-  let [msg, setMsg] = useState("");
-  let [name, setName] = useState("");
-  let [pass, setPass] = useState("");
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
   return (
     <div className="bg-slate-200 rounded-md mt-11 w-[400px] h-[600px] shadow-md pt-5 pb-7 mx-auto p-4">
       <h1 className="text-3xl font-bold text-center text-blue-500 italic">
@@ -29,7 +28,7 @@ function App() {
       />
       <button
         className="outline-1 outline-blue-300 outline-offset-0 bg-blue-500 hover:bg-blue-400 border-0 rounded-md text-2xl ml-2 mt-4 py-4 w-[350px] text-white font-bold"
-        onClick={function (e) {
+        onClick={function () {
           fetch("https://httpbin.org/post", {
             method: "POST",
             body: `username=${name}&password=${pass}`,
